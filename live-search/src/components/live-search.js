d7c6bb5fb27aace9b5b8eb43841f0eb8a0fb4890
Arun Seehra <seehra.a@gmail.com>
2018-11-06 13:27:33 -0600

    Convert to stateful component

--- live-search/src/components/live-search.js
+++ live-search/src/components/live-search.js
@@ -4,12 +4,18 @@ import SearchForm from './search-form';
 import CharacterCount from './character-count';
 import CharacterList from './character-list';
 
-export default function LiveSearch(props) {
+export default class LiveSearch extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
     return (
-        <div className="live-search">
-            <SearchForm />
-            <CharacterCount count={props.characters.length} />
-            <CharacterList characters={props.characters} />
-        </div>
+      <div className="live-search">
+        <SearchForm />
+        <CharacterCount count={this.props.characters.length} />
+        <CharacterList characters={this.props.characters} />
+      </div>
     );
+  }
 }
