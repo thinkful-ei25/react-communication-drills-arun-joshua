21f596faf86da7968e045271739b0511f02542ab
Arun Seehra <seehra.a@gmail.com>
2018-11-06 20:42:26 -0600

    Save old race condition bug for discussion

--- rate-calculator/src/components/rate-calculator.js
+++ rate-calculator/src/components/rate-calculator.js
@@ -8,18 +8,25 @@ export default class RateCalculator extends React.Component {
         super(props);
         this.state = {
             dailyRate: 20,
-            numHours: 4
+            numHours: 4,
+            hourlyRate: 5,
         }
     }
 
     updateDailyRate(dailyRate){
         console.log('daily rate is ' + dailyRate);
         this.setState({dailyRate});
+        this.updateHourlyRate();
     }
 
     updateHours(numHours){
         console.log('num hours is ' + numHours);
         this.setState({numHours});
+        this.updateHourlyRate();
+    }
+
+    updateHourlyRate() {
+        this.setState({ hourlyRate: this.state.dailyRate / this.state.numHours });
     }
 
     calculateHourlyRate(){
@@ -32,7 +39,7 @@ export default class RateCalculator extends React.Component {
             <form>
                 <NumberInput id="day-rate" label="Day rate" min={0} max={5000} onChange={rate=> this.updateDailyRate(rate)} value={this.state.dailyRate}/>
                 <NumberInput id="hours" label="Hours" min={1} max={12} onChange={hours => this.updateHours(hours)} value={this.state.numHours}/>
-                <Output id="hourly-rate" label="Hourly rate" value={this.calculateHourlyRate().toFixed(2)}/>
+                <Output id="hourly-rate" label="Hourly rate" value={this.state.hourlyRate.toFixed(2)}/>
             </form>
         );
     }
